4d7e7b815a3ce8bc66f42bde283b63538c5320ff
AislinSilvers <165269745+AislinSilvers@users.noreply.github.com>
2026-02-19 10:16:00 +0000

    puzzle work in progess 2
    not really working but working enough for now

--- SPS-PenguinPatrol/Assets/Scripts/Cybelle/GameManager.cs
+++ SPS-PenguinPatrol/Assets/Scripts/Cybelle/GameManager.cs
@@ -5,10 +5,11 @@ public class GameManager : MonoBehaviour
 {
 
     //[SerializeField] List<GameObject> puzzleStones = new List<GameObject>();
-  void OnCollisionEnter (Collision Collision)
+  public void OnTriggerEnter(Collider other)
     {
         Debug.Log("hit");
-        if (Collision.gameObject.tag == "puzzleStep1")
+
+        if (other.gameObject.tag == "puzzleStep1")
         {
             Debug.Log("correct!");
             GameObject.Find("Step1").GetComponent<Renderer>().material.color = Color.yellow;
