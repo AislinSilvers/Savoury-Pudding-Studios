4ae214aa19968dc631e27f3fda6d63b1f26b5dd3
AislinSilvers <165269745+AislinSilvers@users.noreply.github.com>
2026-02-24 12:56:46 +0000

    added video ref

--- SPS-PenguinPatrol/Assets/Scripts/Cybelle/DataPersistance/Data/GameData.cs
+++ SPS-PenguinPatrol/Assets/Scripts/Cybelle/DataPersistance/Data/GameData.cs
@@ -7,6 +7,10 @@ using UnityEngine;
 //will update here as needed other data scripts also use this information, however to keep thing clear and clean will only have videos here for these, if add more save and load 
 //featuers will add them on relatice scripts, will need to make a read me ot note will all the video together or an excel sheet. 
 
+//the second video about the between scenes destorys the old scene, if we want players to be able to go back to old scenes that cannot be used so i am adding a potiontal way
+//to do saving/loading without destorying the scene https://www.youtube.com/watch?v=JFP-cCFID7o need to go over it in depth. 
+
+
 [System.Serializable]
 public class GameData 
 {
