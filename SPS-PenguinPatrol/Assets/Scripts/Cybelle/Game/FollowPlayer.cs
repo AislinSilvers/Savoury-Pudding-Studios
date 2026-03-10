f008034c21a189de811bcc4260a13045d6cf357b
AislinSilvers <165269745+AislinSilvers@users.noreply.github.com>
2026-03-10 13:07:03 +0000

    fixed baby
    now done done

--- SPS-PenguinPatrol/Assets/Scripts/Cybelle/Game/FollowPlayer.cs
+++ SPS-PenguinPatrol/Assets/Scripts/Cybelle/Game/FollowPlayer.cs
@@ -6,24 +6,40 @@ public class FollowPlayer : MonoBehaviour
     public GameObject playerObject;
 
     public float speed = 1f;
+    public bool follow;
     
     
 
 
      void Start()
     {
-       
+        follow = false;   
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("follow");
+            follow = true;
+        }
+
+
     }
 
     void FixedUpdate()
     { 
+        
        float dist = Vector3.Distance(chickObject.transform.position, playerObject.transform.position);
-       if(dist > 2)
+       if(dist > 2 && follow)
         {
             chickObject.transform.position = Vector3.MoveTowards(chickObject.transform.position, playerObject.transform.position, speed*Time.deltaTime);
         }
 
     }
+
+
 
     
 }
