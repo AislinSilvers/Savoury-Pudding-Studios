a52538a4f136f70646ce7ce390e4e4574ce04b57
felice <felicabenar.uk@gmail.com>
2026-03-11 06:32:38 +0000

    Volume Settings script (!!!)
    Sliders work, change the value in audio mixer window, but not in game when actually playing. not in menu scene nor cave

--- SPS-PenguinPatrol/Assets/Scripts/Felicia/VolumeSettings.cs
+++ SPS-PenguinPatrol/Assets/Scripts/Felicia/VolumeSettings.cs
@@ -2,28 +2,47 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
+//code from tutorial https://www.youtube.com/watch?v=G-JUp8AMEx0
+
 public class VolumeSettings : MonoBehaviour
 {
     [SerializeField] private AudioMixer mainMixer;
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider soundFXSlider;
 
-    /*private void Start()
+    private void Start()
     {
-        SetMusicVolume();    
-        SetSFXVolume();
-    }*/
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+            SetSFXVolume();
+        }
+    }
 
     public void SetMusicVolume()
     {
         float volume = musicSlider.value;
         mainMixer.SetFloat("Music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
     }
 
     public void SetSFXVolume()
     {
         float volume = soundFXSlider.value;
         mainMixer.SetFloat("SoundFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("soundFXVolume", volume);
     }
 
+    private void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("Music");
+        soundFXSlider.value = PlayerPrefs.GetFloat("SoundFX");
+
+        //SetMusicVolume;
+        //SetSFXVolume;
+    }
 }
