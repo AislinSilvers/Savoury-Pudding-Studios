3cab1dbebc03283ecd8d2814db8bdb7b5ae81d00
felice <felicabenar.uk@gmail.com>
2026-03-12 05:05:07 +0000

    Brightness slider & volume
    Brightness light created, to change the value > ui > settings > brightness slider
    
    Music and SFX sliders set to 0.5 by default

--- SPS-PenguinPatrol/Assets/Scripts/Felicia/BrightnessChange.cs
+++ SPS-PenguinPatrol/Assets/Scripts/Felicia/BrightnessChange.cs
@@ -1,13 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BrightnessChange : MonoBehaviour
 {
-    [SerializeField] Slider brightnessSlider;
-    [SerializeField] Light sceneLight;
+    [SerializeField] public Slider slider;
+    [SerializeField] public Light sceneLight;
 
-    void OnSlider()
+    void Start()
     {
-        sceneLight.intensity = brightnessSlider.value;
+        
+    }
+    void Update()
+    {
+        sceneLight.intensity = slider.value * 2;
     }
 }
