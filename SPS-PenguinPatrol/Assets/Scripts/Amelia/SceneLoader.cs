9228767db2dca0ef349d9b465c722673fef952a6
Amelia Ewing <amelia.ewing@soluis.com>
2026-03-12 22:48:01 +0000

    Fixed load screen thing

--- SPS-PenguinPatrol/Assets/Scripts/Amelia/SceneLoader.cs
+++ SPS-PenguinPatrol/Assets/Scripts/Amelia/SceneLoader.cs
@@ -4,13 +4,15 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
     public string loadScene = "Load";
+   
+    public string currentScene = "Antarctica";
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            // save which scene we are coming from so load screen knows where to go next
-            PlayerPrefs.SetString("LastScene", "Highlands");
+          
+            PlayerPrefs.SetString("LastScene", currentScene);
             SceneManager.LoadScene(loadScene);
         }
     }
