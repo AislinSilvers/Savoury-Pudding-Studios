ac029c442c58a952122240aac232d915fd87cac4
felice <felicabenar.uk@gmail.com>
2026-03-12 09:16:44 +0000

    Object Look At Camera & NPC
    FaceCamera Script that will be attached to an object so that object always faces MainCamera (created for NPC since all of those are 2D sprites)
    
    starting on NPCs. Owl NPC exists in the game, has collider and looks at the camera
    
    next up: take a nap :D then create UI dialogue layout and start working on triggering a dialogue (Player getting withing range of NPC's capsule collider and enabling a dialogue on button Q/E ?? or triggering a dialogue once in capsule collider range)

--- SPS-PenguinPatrol/Assets/Scripts/Felicia/FaceCamera.cs
+++ SPS-PenguinPatrol/Assets/Scripts/Felicia/FaceCamera.cs
@@ -3,6 +3,13 @@ using UnityEngine.UI;
 
 public class FaceCamera : MonoBehaviour
 {
+    //nice tutorial https://youtu.be/rOVLui_twYI?si=zg1g479NxtJ7AizP
+    private void LateUpdate()
+    {
+        transform.LookAt(Camera.main.transform); //whatever this code is linked to (any NPC) will always face camera
+    }
+
+
     /*
     //code from semester 2. it was used to make player's name always face the camera. in this project it makes npcInstance fly around tho
     [SerializeField] Transform npcInstance;
@@ -18,6 +25,9 @@ public class FaceCamera : MonoBehaviour
     }
     */
 
+
+    /*
+     //another code but for 'player' to alwya face camera, prob not for npcs https://youtube.com/shorts/KGG2V4ZkXTg?si=ITPHerr4NF8K5YuW 
     [SerializeField] private Camera _mainCamera;
 
     private void LateUpdate()
@@ -29,6 +39,7 @@ public class FaceCamera : MonoBehaviour
         transform.LookAt(cameraPosition);
         transform.Rotate(0f, 180f, 0f);
     }
+    */
 
 
 }
