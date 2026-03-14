6068e7e918d464cab8649c826a07bf125d5fd3f7
( <avanyaheyworth@hotmail.com>
2026-03-14 16:07:07 +0000

    inventory and shop system
    not done yet, but got the shop part working mostly
    can "buy" an item

--- SPS-PenguinPatrol/Assets/Scripts/Avanya/inventory.cs
+++ SPS-PenguinPatrol/Assets/Scripts/Avanya/inventory.cs
@@ -3,22 +3,25 @@ using UnityEngine;
 public class inventory : MonoBehaviour
 {
     [SerializeField] GameObject accessory;
-    bool accessoryOn;
+        bool accessoryOn;
 
-    // void Start()
-    // {
-    //     if (accessory = null) 
-    //     {
-    //              accessory.SetActive (false);
-    //          }
-    // }
+        //buying items variables
+    public rubbishCollection litterScript;
+    public int money; 
+    int currency;
+
+
+    //GameObject script;
+
+    void Start()
+    {
+        litterScript = GameObject.Find("Player").GetComponent<rubbishCollection>();
+    }
 
     public void toggleAccessory()
     {
         accessoryOn = !accessoryOn;
-        Debug.Log(accessoryOn);
-
-
+        //Debug.Log(accessoryOn);
              if (accessoryOn == true)
         {
             accessory.SetActive(true);
@@ -34,5 +37,21 @@ public class inventory : MonoBehaviour
     {
         accessoryOn = false;
         accessory.SetActive(false);
+    }
+
+    
+    public void buyItem1()
+    {
+       litterScript.BuyItem1();
+    }
+
+    public void buyItem2()
+    {
+       litterScript.BuyItem2();
+    }
+
+    public void buyItem3()
+    {
+       litterScript.BuyItem3();
     }
 }
--- SPS-PenguinPatrol/Assets/Scripts/Avanya/rubbishCollection.cs
+++ SPS-PenguinPatrol/Assets/Scripts/Avanya/rubbishCollection.cs
@@ -6,13 +6,31 @@ public class rubbishCollection : MonoBehaviour
     //https://www.youtube.com/watch?v=6iSJ_jh6Rdo
 
 
-    private int Currency = 0;
+    [SerializeField] public int Currency = 0;
 
-    [SerializeField] int price = 4;
+    [SerializeField] int price = 15;
+
+    public GameObject hat1Button;
+    public GameObject hat2Button;
+    public GameObject hat3Button;
+
+    public bool hat1Bought;
+    public bool hat2Bought;
+    public bool hat3Bought;
+    
 
     public TextMeshProUGUI currencyText;
 
+    void Start()
+    {
+        Currency = 20;
+        
+            currencyText.text = Currency.ToString();
+            
+        
+    }
 
+// adds 1 to the litter counter on collision with an object tagged as "rubbish"
     private void OnTriggerEnter(Collider other)
     {
         if(other.transform.tag == "rubbish")
@@ -23,9 +41,64 @@ public class rubbishCollection : MonoBehaviour
         }
     }
 
-    public void buyItem()
+    public void BuyItem1()
     {
-        //nothing for now
+        Debug.Log("hat2");
+
+        if (Currency >= price)
+        {
+            Currency -= price;
+            hat1Button.SetActive(true);
+        }
+        else
+        {
+            return;
+        }
+
+        currencyText.text = Currency.ToString();
+
+           Debug.Log(Currency);
     }
+
+
+       public void BuyItem2()
+    {
+        Debug.Log("hat2");
+
+        if (Currency >= price)
+        {
+            Currency -= price;
+            hat2Button.SetActive(true);
+        }
+        else
+        {
+            return;
+        }
+
+        currencyText.text = Currency.ToString();
+
+           Debug.Log(Currency);
+    }
+
+       public void BuyItem3()
+    {
+        Debug.Log("hat3");
+
+        if (Currency >= price)
+        {
+            hat3Button.SetActive(true);
+        }
+        else
+        {
+            return;
+        }
+
+        currencyText.text = Currency.ToString();
+
+           Debug.Log(Currency);
+    }
+
+
+    //buy item - this much currency 
     
 }
