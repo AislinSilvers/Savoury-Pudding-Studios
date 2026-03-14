3169b75144a070e3df4099114bc39d6db94c948c
felice <felicabenar.uk@gmail.com>
2026-03-14 01:38:30 +0000

    figuring out chat bubble
    for some reason works for 1 npc but not the other. possible issue: image sizes
    
    next:
    (priority) write all dialogues
    make nice bubble that works for all NPCs
    check if gamepad works with dialogues

--- SPS-PenguinPatrol/Assets/Scripts/Felicia/NPC/NPC_Talk.cs
+++ SPS-PenguinPatrol/Assets/Scripts/Felicia/NPC/NPC_Talk.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class NPC_Talk : MonoBehaviour
 {
     private BoxCollider boxCollider;
+    public Animator interactAnimator;
     public DialogueSO dialogueSO;
     public bool dialogueActivated;
+
 
     private void Awake()
     {
         boxCollider = GetComponent<BoxCollider>();
+        interactAnimator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
@@ -28,12 +31,14 @@ public class NPC_Talk : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             dialogueActivated = true;
+            interactAnimator.Play("Open");
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
         dialogueActivated = false;
+        interactAnimator.Play("Close");
     }
 
 
