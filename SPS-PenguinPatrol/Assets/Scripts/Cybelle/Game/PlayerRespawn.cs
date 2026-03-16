c3893dd36fc8fb3991edf1414dc6de118d2a1394
AislinSilvers <165269745+AislinSilvers@users.noreply.github.com>
2026-03-16 12:59:12 +0000

    player posittion fixed
    made the things work

--- SPS-PenguinPatrol/Assets/Scripts/Cybelle/Game/PlayerRespawn.cs
+++ SPS-PenguinPatrol/Assets/Scripts/Cybelle/Game/PlayerRespawn.cs
@@ -3,34 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 //got from https://www.youtube.com/watch?v=W7UGintd7ek
 //this is old code from prevouse game, deleted stuff not needed. rewated video to make sure it works as intended.
-//updated to save last safe position so player respawns where they last were before hitting water
+
 public class PlayerRespawn : MonoBehaviour
 {
     public Vector3 respawnPoint;
-    private Vector3 lastSafePosition;
+    public Vector3 lastSafePosition;
 
-    void Start()
-    {
-        // set last safe position to starting position at the beginning
-        lastSafePosition = transform.position;
-    }
-
-    void Update()
-    {
-        // keep saving position every frame as long as player is on safe ground
-        // only updates when not in water
-        if (!inWater)
-        {
-            lastSafePosition = transform.position;
-        }
-    }
-
-    private bool inWater = false;
 
     public void RespawnNow()
     {
         transform.position = lastSafePosition;
-        inWater = false;
+        
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -41,20 +24,5 @@ public class PlayerRespawn : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Water")
-        {
-            inWater = true;
-            RespawnNow();
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Water")
-        {
-            inWater = false;
-        }
-    }
+   
 }
