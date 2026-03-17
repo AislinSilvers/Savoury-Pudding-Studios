70099a5759bb47239adc227874cc0080618c4a7c
Amelia Ewing <amelia.ewing@soluis.com>
2026-03-17 03:18:35 +0000

    added settings for portals to next biomes

--- SPS-PenguinPatrol/Assets/Scripts/Amelia/SceneLoader.cs
+++ SPS-PenguinPatrol/Assets/Scripts/Amelia/SceneLoader.cs
@@ -5,16 +5,36 @@ public class SceneLoader : MonoBehaviour
 {
     public string loadScene = "Load";
     public string currentScene = "Antarctica";
+    public bool isFinalScene = false;
+    public GameObject endGamePanel;
+
+    void Start()
+    {
+        // portal hidden until baby penguin found
+        gameObject.SetActive(false);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            // save game data before leaving the scene
-            DataPersistenceManager.instance.SaveGame();
-
-            PlayerPrefs.SetString("LastScene", currentScene);
-            SceneManager.LoadScene(loadScene);
+            if (isFinalScene)
+            {
+                // show end game panel instead of loading next scene
+                if (endGamePanel) endGamePanel.SetActive(true);
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                DataPersistenceManager.instance.SaveGame();
+                PlayerPrefs.SetString("LastScene", currentScene);
+                SceneManager.LoadScene(loadScene);
+            }
         }
+    }
+
+    public void ActivatePortal()
+    {
+        gameObject.SetActive(true);
     }
 }
